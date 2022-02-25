08ccf5338d855dcac2fb56163acb87d22d11474d
Jeremy West <jeremywest2000@gmail.com>
2022-02-25 21:00:00 +1100

    news list toggling working
    ready to implement map data from backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,41 @@
+import { Component } from "react";
 import Feed from "./Components/Feed/Feed";
 import NewsList from "./Components/NewsList";
 
-function App() {
-  return (
-    <div>
-      <NewsList/>
-    </div>
-  )
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "React",
+      showNewsList: true,
+    };
+    this.toggleComponent = this.toggleComponent.bind(this);
+  }
+
+  toggleComponent(name) {
+    console.log(name);
+    switch (name) {
+      case "showNewsList":
+        console.log("Toggling")
+        this.setState({ showNewsList: !this.state.showNewsList });
+        break;
+      default:
+        console.log("defualt");
+    }
+  }
+
+  render() {
+    const {showNewsList} = this.state;
+
+    return (
+      <div>
+        <button onClick={() => this.toggleComponent("showNewsList")}>toggle showNewsList(replace with map coutnry thing)</button>
+        {showNewsList && <NewsList onClick={() => this.toggleComponent("showNewsList")}/>}
+      </div>
+      
+    );
+  }
 }
 
 export default App;
--- frontend/src/Components/NewsList.js
+++ frontend/src/Components/NewsList.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import NewsItem from './NewsItem'
 
-const NewsList = () => {
+const NewsList = (props) => {
     return (
         <div className="py-10 flex justify-center">  
             <div className="bg-sky-100 max-w-md rounded overflow-hidden shadow-lg">    
                 <div className="px-6 py-4">
                     <div className="font-bold text-xl mb-2">
-                        News in (coutry selected)
+                        
+                        <h2>2 News in (coutry selected)</h2>
+                        <button onClick={props.onClick}>Exit</button>
                     </div>
                     <NewsItem/>
                 </div>
