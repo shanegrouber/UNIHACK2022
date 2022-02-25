68bbb9d1872024a9d65b4abb04541d3e45946005
tron2790 <jasonstuart36@gmail.com>
2022-02-25 20:58:05 +1030

    Feed page created
    Feed page created (Skeleton)
    
    -Needs CSS

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,10 @@
+import Feed from "./Components/Feed";
+
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="text-3xl font-bold">
+      <Feed/>
+    </div>
   )
 }
 
--- frontend/src/Components/Feed.js
+++ frontend/src/Components/Feed.js
@@ -2,7 +2,23 @@ import React from 'react'
 
 const Feed = () => {
   return (
-    <div>This is the feed component NEEDS WORK</div>
+    <div class="bg-sky-200 rounded-lg shadow-2xl shadow-lg shadow-sky-500/50 border-solid  border-0 border-blue-700 w-64 h-96 "> 
+    
+    
+    <div class="flex flex-col ">
+      <h1 class="text-center antialiased font-bold tracking-wide underline decoration-2 underline-offset-4 ">News Feed</h1> 
+
+
+    </div>
+    <div class="divide-y-8 pb-2  "></div>
+    
+      <div class="grid grid-cols-1 gap-4 overflow-y-auto h-80 ">
+        <div class=" rounded-md border-black border-0 w-48 h-24 justify-self-center shadow-md bg-slate-300 shadow-slate-500/50  "></div>
+        <div class=" rounded-md border-black border-0 w-48 h-24 justify-self-center shadow-md bg-slate-300 shadow-slate-500/50 "></div>
+        <div class=" rounded-md border-black border-0 w-48 h-24 justify-self-center shadow-md bg-slate-300 shadow-slate-500/50 "></div>
+        <div class=" rounded-md border-black border-0 w-48 h-24 justify-self-center shadow-md bg-slate-300 shadow-slate-500/50 "></div>
+    </div>
+    </div>
   )
 }
 
