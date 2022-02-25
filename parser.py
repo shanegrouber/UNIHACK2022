cdca3d33bc1703b80c22c778f043583bade5912c
andrewapp <andrew.appuhamy@gmail.com>
2022-02-26 00:28:45 +1100

    parse titles, desc, sources

--- parser.py
+++ parser.py
@@ -5,5 +5,18 @@ in_file = open("top-headlines.json")
 
 data = json.load(in_file)
 
-print(i['title'] for i in data[]
 
+titles = [i['title'] for i in data['articles']]
+
+descriptions = [i['description'] for i in data['articles']]
+
+sources = [i['source'] for i in data['articles']]
+
+for i in range (0,len(sources)):
+    sources[i] = sources[i]['name']
+
+print(titles)
+print(" ")
+print(descriptions)
+print(" ")
+print(sources)
