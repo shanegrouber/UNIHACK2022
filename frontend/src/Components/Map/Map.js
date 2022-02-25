fcefeebd3765af459e7ded13da2a8c79a05b32e1
NikitaZaytsev1907 <nikitazaytsev1907@gmail.com>
2022-02-26 03:14:59 +1100

    its broken and I hate it

--- frontend/src/Components/Map/Map.js
+++ frontend/src/Components/Map/Map.js
@@ -1,11 +1,29 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
+import ReactDOM from 'react-dom';
 import { Circle, CircleMarker, MapContainer, Polyline, Polygon, Popup, Rectangle, TileLayer,} from 'react-leaflet';
 import './Map.css';
 
 const Map = () => {
   const position = [50.447731, 30.542721];
   const [map, setMap] = useState(null);
-  
+
+  // Test Data for placing circles
+  const circleData = [
+    {"latlng": [51, 31], "articles": 10},
+    {"latlng": [50, 30], "articles": 100},
+    {"latlng": [52, 32], "articles": 1},
+    {"latlng": [49, 29], "articles": 1000},
+  ];
+  const Circles = [];
+  function DrawCircle() {
+    
+    //circleData.forEach(point => {
+    //  
+    //});
+    return <Circle center={circleData[0].latlng} pathOptions={fillRedOptions} radius={circleData[0].articles * 10}></Circle>
+  }
+  ReactDOM.render(<DrawCircle />, document.getElementById('circles'))
+
   const [coords, setCoords] = useState({});
   
   const fillRedOptions = { fillColor: 'red' , color: 'red'}
@@ -30,9 +48,12 @@ const Map = () => {
           url="https://api.mapbox.com/styles/v1/andrew-app/cl02c6cxf005a15oirym7wn2a/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiYW5kcmV3LWFwcCIsImEiOiJjbDAxcjlzdWwwaTMyM3Fyc3JreDBqZGI4In0.QH_An0n-PwVGGbQGVSCWfQ"
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors Imagery © <a href="https://www.mapbox.com/">Mapbox</a>'
         />
-        <Circle center={position} pathOptions={fillRedOptions} radius={200}>
-        <Popup>Popup in CircleMarker</Popup>
+        
+        
+        <Circle center={position} pathOptions={fillRedOptions} radius={2000}>
+        <Popup>Popup in Circle</Popup>
         </Circle>
+        <div id="circles"></div>
 
       </MapContainer>
       {lat && (
