8a2f378c2755f26740b5112a7d7dfa8a4063c34f
NikitaZaytsev1907 <nikitazaytsev1907@gmail.com>
2022-02-26 01:30:00 +1100

    Update Map.js

--- frontend/src/Components/Map/Map.js
+++ frontend/src/Components/Map/Map.js
@@ -1,13 +1,15 @@
 import { useEffect, useState } from "react";
-import { MapContainer, TileLayer} from 'react-leaflet';
+import { Circle, CircleMarker, MapContainer, Polyline, Polygon, Popup, Rectangle, TileLayer,} from 'react-leaflet';
 import './Map.css';
 
 const Map = () => {
-  const position = [51.505, -0.09];
+  const position = [50.447731, 30.542721];
   const [map, setMap] = useState(null);
   
   const [coords, setCoords] = useState({});
   
+  const fillRedOptions = { fillColor: 'red' , color: 'red'}
+
   useEffect(() => {
     if (!map) return;
   
@@ -20,7 +22,7 @@ const Map = () => {
   
   return (
     <div>
-      <MapContainer center={[50.447731, 30.542721]} zoom={13}
+      <MapContainer zoom={13}
         center={position}
         whenCreated={setMap}
       >
@@ -28,6 +30,10 @@ const Map = () => {
           url="https://api.mapbox.com/styles/v1/andrew-app/cl02c6cxf005a15oirym7wn2a/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiYW5kcmV3LWFwcCIsImEiOiJjbDAxcjlzdWwwaTMyM3Fyc3JreDBqZGI4In0.QH_An0n-PwVGGbQGVSCWfQ"
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors Imagery © <a href="https://www.mapbox.com/">Mapbox</a>'
         />
+        <Circle center={position} pathOptions={fillRedOptions} radius={200}>
+        <Popup>Popup in CircleMarker</Popup>
+        </Circle>
+
       </MapContainer>
       {lat && (
         <div>
