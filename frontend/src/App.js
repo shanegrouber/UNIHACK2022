7866e9dd7e160f8542527ad90dfad481d9ec0811
NikitaZaytsev1907 <nikitazaytsev1907@gmail.com>
2022-02-25 22:07:31 +1100

    Navbar added
    Added navbar skeleton

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import Feed from "./Components/Feed/Feed";
 import NewsList from "./Components/NewsList";
+import Header from "./Components/Header";
 
 function App() {
   return (
     <div>
+      <Header/>
       <NewsList/>
     </div>
   )
