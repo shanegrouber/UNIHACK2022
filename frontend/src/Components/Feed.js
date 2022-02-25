1c4336b3231e9b322b7c6a06e470c0d1e61363c3
Jeremy West <jeremywest2000@gmail.com>
2022-02-25 19:09:21 +1100

    Cleaned up boilerplate
    got rid of useless stuff

---
+++ frontend/src/Components/Feed.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Feed = () => {
+  return (
+    <div>This is the feed component NEEDS WORK</div>
+  )
+}
+
+export default Feed
---
+++ frontend/src/Components/Map.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Map = () => {
+  return (
+    <div>This is the map component NEEDS WORK</div>
+  )
+}
+
+export default Map
