719414ca0e1435b8a951ac923265d4f4be5d0dd3
andrewapp <andrew.appuhamy@gmail.com>
2022-02-26 12:44:10 +1100

    filter locations to json

--- frontend/src/Components/Map/Map.js
+++ frontend/src/Components/Map/Map.js
@@ -2,14 +2,15 @@ import React from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import './Map.css';
 const Map = () => {
+  const position = [50.447731, 30.542721]
   return (
     <div>
-      <MapContainer center={[50.447731, 30.542721]} zoom={13}>
+      <MapContainer center={position} zoom={13}>
       <TileLayer
         url="https://api.mapbox.com/styles/v1/andrew-app/cl02c6cxf005a15oirym7wn2a/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiYW5kcmV3LWFwcCIsImEiOiJjbDAxcjlzdWwwaTMyM3Fyc3JreDBqZGI4In0.QH_An0n-PwVGGbQGVSCWfQ"
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors Imagery © <a href="https://www.mapbox.com/">Mapbox</a>'
       />
-      <Marker position={[50.447731, 30.542721]}>
+      <Marker position={position}>
         <Popup>
           A pretty CSS3 popup. <br /> Easily customizable.
         </Popup>
--- locfilter/parser.py
+++ locfilter/parser.py
@@ -1,7 +1,9 @@
 import requests
 import json
+import numpy as np
+import pandas as pd
 
-in_file = open("top-headlines.json")
+in_file = open('top-headlines.json')
 
 data = json.load(in_file)
 
@@ -17,13 +19,53 @@ content = [i['content'] for i in data['articles']]
 for i in range (0,len(sources)):
     sources[i] = sources[i]['name']
 
-print(titles)
-print(" ")
-print(descriptions)
-print(" ")
-print(content)
-print(" ")
+titles = np.array(titles)
+
+descriptions = np.array(descriptions)
+
+sources = np.array(sources)
+
+contents = np.array(content)
+
+
+contents = contents[contents != None] #remove nonetype elements in array
+
+df = pd.read_csv('cities.csv')
+df['count'] = 0
+cities = df['city']
+
+df.set_index('city', inplace=True, drop=True)
+
 print(sources)
+
+
+for city in cities:
+    for title in titles:
+        isSource = False
+        for source in sources:
+            if source in title:
+                isSource = True
+        if city in title:
+            if isSource is False:
+                df.loc[city,'count'] += 1
+    
+    for desc in descriptions:
+        if city in desc:
+            df.loc[city,'count'] += 1
+    
+    for cont in contents:
+        if city in cont:
+            df.loc[city,'count'] += 1
+
+
+df=df[df!=0].dropna()
+
+
+out = df.to_json(orient='records')[1:-1].replace('},{', '} {')
+
+print(out)
+# df.to_csv('filtered.csv')
+
 
 in_file.close()
 
