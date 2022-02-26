37d5546267f350aedd9f9b0f0a9572a7e1cb89ed
Jeremy West <jeremywest2000@gmail.com>
2022-02-26 11:38:52 +1100

    long and lat passing to app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,49 +1,33 @@
-import { Component } from "react";
+import { Component, useState} from "react";
 import Feed from "./Components/Feed";
 import NewsList from "./Components/NewsList";
 import Header from "./Components/Header";
 import Map from "./Components/Map/Map";
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      name: "React",
-      showNewsList: true,
-    };
-    this.toggleComponent = this.toggleComponent.bind(this);
+const App = () => {
+
+  const [coords, setData] = useState(null);
+
+  const childToParent = (childdata) => {
+    setData(childdata);
   }
 
-  toggleComponent(name) {
-    console.log(name);
-    switch (name) {
-      case "showNewsList":
-        console.log("Toggling")
-        this.setState({ showNewsList: !this.state.showNewsList });
-        break;
-      default:
-        console.log("defualt");
-    }
-  }
-
-  render() {
-    const {showNewsList} = this.state;
-
-    return (
-      <div>
-        <Header/>
-        <div class="grid grid-cols-4 gap-0 pt-8">
-          <div class="col-span-3">
-            <Map/>
-          </div>
-          <Feed/>
+  return (
+    <div>
+      <Header/>
+      <div class="grid grid-cols-4 gap-0 pt-8">
+        <div class="col-span-3">
+          <Map childToParent={childToParent}/>
+          {coords}
         </div>
-        {/* <button onClick={() => this.toggleComponent("showNewsList")}>||News list toggle||</button>
-        {showNewsList && <NewsList onClick={() => this.toggleComponent("showNewsList")}/>} */}
+        <Feed/>
+        
       </div>
-      
-    );
-  }
+      {/* <button onClick={() => this.toggleComponent("showNewsList")}>||News list toggle||</button>
+      {showNewsList && <NewsList onClick={() => this.toggleComponent("showNewsList")}/>} */}
+    </div>
+    
+  );
 }
 
 export default App;
