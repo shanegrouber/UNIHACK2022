3e9842763dec8043226281899fd180531ea9da74
Jeremy West <jeremywest2000@gmail.com>
2022-02-26 20:22:56 +1100

    circle cities working

--- frontend/src/Components/Map/Map.js
+++ frontend/src/Components/Map/Map.js
@@ -11,36 +11,44 @@ const Map = ({childToParent}) => {
   const [coords, setCoords] = useState({lng: 0, lat: 0});
   const fillRedOptions = { fillColor: 'red' , color: 'red'}
   const [circles, setCircles] = useState([]);
-
+  const [location, setLocation] = useState("");
+  
   useEffect(() => {
     setCircles(data)
     if (!map) return;
     map.addEventListener("click", (e) => {
       setCoords({ lat: e.latlng.lat, lng: e.latlng.lng });
-      console.log("coords")
     });
   }, [map]);
-  const textCoords =coords.lng.toString() + "," + coords.lat.toString();
-        
+
+  const getCircleLocation = (data) => {
+    setLocation(data);
+  }
+
   return (
     <div>
-      <MapContainer zoom={13} center={position} whenCreated={setMap}>
+      <MapContainer zoom={13} center={position} whenCreated={setMap} on>
         <TileLayer
           url="https://api.mapbox.com/styles/v1/andrew-app/cl02c6cxf005a15oirym7wn2a/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiYW5kcmV3LWFwcCIsImEiOiJjbDAxcjlzdWwwaTMyM3Fyc3JreDBqZGI4In0.QH_An0n-PwVGGbQGVSCWfQ"
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors Imagery © <a href="https://www.mapbox.com/">Mapbox</a>'
         />
 
         {Object.keys(circles).map((key, i ) => (
-          <Circle key={i} center={[circles[key].lat, circles[key].long]} pathOptions={fillRedOptions} radius={circles[key].count * 100000}>
+          <Circle 
+            key={i} 
+            center={[circles[key].lat, circles[key].long]} 
+            pathOptions={fillRedOptions} 
+            radius={circles[key].count * 100000} 
+            eventHandlers={{click: (e) => {
+              console.log(circles[key].lat)
+              getCircleLocation(circles[key].city)
+            },
+          }}>
             <Popup>Popup in Circle</Popup>
-            {childToParent(coords)}
           </Circle>
         ))}
-
-        <div id="circles"></div> 
-
+      {childToParent(location)}
       </MapContainer>
-      {childToParent(textCoords)}
     </div>
   );
 };
