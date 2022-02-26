76c1ea8ef9d4e4695a434d94efcb705b5e6d8cf0
tron2790 <jasonstuart36@gmail.com>
2022-02-26 21:42:49 +1030

    Update PhoneSMS.js
    Updated the look for the Twilio

--- frontend/src/Components/PhoneSMS.js
+++ frontend/src/Components/PhoneSMS.js
@@ -6,7 +6,9 @@ class PhoneSMS extends Component{
 
   state = {
     text:{
-      recipient: ''
+      recipient: '',
+      city: '',
+      countryName: ''
     }
   }
   sendText = _ => {
@@ -21,12 +23,23 @@ class PhoneSMS extends Component{
   render(){
     const { text } = this.state;
     return(
-    <div >
-      <h2 className='text-sm text-justify'>Keep up to date with the news!</h2>
-    <input className="bg-slate-300 border border-sky-400  rounded-lg hover:shadow-xl focus:border-sky-500 focus:border-2 focus:ring-1 focus:ring-sky-500 focus:outline-none shadow-sm 
-"   placeholder='e.g 0456755098'   value={text.recipint} onChange={e => this.setState({ text: {...text, recipient: e.target.value}})}></input>
-    <button className=' bg-indigo-400 rounded-lg font-bold transition ease-in-out delay-60 bg-blue-500  hover:scale-110 hover:bg-indigo-500 duration-300 pl-4 pr-4 ml-2
-    ' onClick={this.sendText}>Subscribe</button>
+    <div className=' shadow-lg w-96 h-96'>
+      <div className='absolute'>
+      <h1 className=''>Get Alerted With News Near You!</h1>
+      </div>
+      <div className='mt-64'>
+          
+          <input className=" ml-0.5 bg-slate-300 border border-sky-400  rounded-lg hover:shadow-xl focus:border-sky-500 focus:border-2 focus:ring-1 focus:ring-sky-500 focus:outline-none shadow-sm 
+    "   placeholder='e.g Adelaide'   value={text.recipint} onChange={e => this.setState({ text: {...text, city: e.target.value}})}></input>
+        <input className=" ml-3 bg-slate-300 border border-sky-400  rounded-lg hover:shadow-xl focus:border-sky-500 focus:border-2 focus:ring-1 focus:ring-sky-500 focus:outline-none shadow-sm 
+    "   placeholder='e.g South Australia'   value={text.recipint} onChange={e => this.setState({ text: {...text, countryName: e.target.value}})}></input>
+    <div className='grid grid-col-1'>
+        <input className=" ml-0.5 mr-0.5 bg-slate-300 border border-sky-400  rounded-lg hover:shadow-xl focus:border-sky-500 focus:border-2 focus:ring-1 focus:ring-sky-500 focus:outline-none shadow-sm 
+    "   placeholder='e.g 0456755098'   value={text.recipint} onChange={e => this.setState({ text: {...text, recipient: e.target.value}})}></input>
+        <button className='mt-2 ml-8 mr-8 bg-indigo-400 rounded-lg font-bold transition ease-in-out delay-60 bg-blue-500  hover:scale-110 hover:bg-indigo-500 duration-300 pl-4 pr-4 ml-2
+        ' onClick={this.sendText}>Enter</button>
+        </div>
+    </div>
     </div>
     );
   }
