4b74b98919546eb96ddf8f979e85a4442d7915d9
Jeremy West <jeremywest2000@gmail.com>
2022-02-26 18:15:03 +1100

    sample json data working

--- frontend/src/Components/Map/Map.js
+++ frontend/src/Components/Map/Map.js
@@ -1,7 +1,10 @@
+import { circle } from "leaflet";
 import React, { useEffect, useState } from "react";
 import ReactDOM from 'react-dom';
 import { Circle, CircleMarker, MapContainer, Polyline, Polygon, Popup, Rectangle, TileLayer,} from 'react-leaflet';
 import './Map.css';
+import data from './sampledata.json';
+
 
 const Map = () => {
   const position = [50.447731, 30.542721];
@@ -9,19 +12,10 @@ const Map = () => {
   const [coords, setCoords] = useState({});
   const fillRedOptions = { fillColor: 'red' , color: 'red'}
   const [circles, setCircles] = useState([]);
-
-
-  // Test Data for placing circles
-  const circleData = [
-    {"latlng": [51, 31], "articles": 10},
-    {"latlng": [50, 30], "articles": 100},
-    {"latlng": [52, 32], "articles": 1},
-    {"latlng": [49, 29], "articles": 1000},
-  ];
+  
 
   useEffect(() => {
-    setCircles(circleData);
-    console.log(circles)
+    setCircles(data)
     if (!map) return;
     map.addEventListener("click", (e) => {
       setCoords({ lat: e.latlng.lat, lng: e.latlng.lng });
@@ -37,13 +31,21 @@ const Map = () => {
           url="https://api.mapbox.com/styles/v1/andrew-app/cl02c6cxf005a15oirym7wn2a/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiYW5kcmV3LWFwcCIsImEiOiJjbDAxcjlzdWwwaTMyM3Fyc3JreDBqZGI4In0.QH_An0n-PwVGGbQGVSCWfQ"
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors Imagery © <a href="https://www.mapbox.com/">Mapbox</a>'
         />
+        {console.log(circles[1].city)}
 
-        <>
+        {Object.keys(circles).map((key, i ) => (
+          <Circle key={i} center={[circles[key].lat, circles[key].long]} pathOptions={fillRedOptions} radius={circles[key].count * 100000}>
+            <Popup>Popup in Circle</Popup>
+          </Circle>
+        ))}
+
+
+        {/* <>
         {circles.map(({latlng,articles}) => (
           <Circle key={latlng} center={latlng} pathOptions={fillRedOptions} radius={articles * 10}>
             <Popup>Popup in Circle</Popup>
           </Circle>
-        ))} </>
+        ))} </> */}
 
         <div id="circles"></div>
 
