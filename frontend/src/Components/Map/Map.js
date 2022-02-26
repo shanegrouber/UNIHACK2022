fe1f9d814fdb7b0e2b1bec9f17d973b79af55f80
NikitaZaytsev1907 <nikitazaytsev1907@gmail.com>
2022-02-26 12:58:38 +1100

    Merge branch 'Heat-Map-(circles)' into Frontend

--- frontend/src/Components/Map/Map.js
+++ frontend/src/Components/Map/Map.js
@@ -1,13 +1,25 @@
-import { useEffect, useState } from "react";
-import { MapContainer, TileLayer} from 'react-leaflet';
+import React, { useEffect, useState } from "react";
+import ReactDOM from 'react-dom';
+import { Circle, CircleMarker, MapContainer, Polyline, Polygon, Popup, Rectangle, TileLayer,} from 'react-leaflet';
 import './Map.css';
 
 const Map = ({childToParent}) => {
-  const position = [51.505, -0.09];
+  const position = [50.447731, 30.542721];
   const [map, setMap] = useState(null);
+
+  // Test Data for placing circles
+  const circleData = [
+    {"latlng": [51, 31], "articles": 10},
+    {"latlng": [50, 30], "articles": 100},
+    {"latlng": [52, 32], "articles": 1},
+    {"latlng": [49, 29], "articles": 1000},
+  ];
+  const Circles = [];
+
+  const [coords, setCoords] = useState({});
   
-  const [coords, setCoords] = useState({lng: 0, lat: 0});
-  
+  const fillRedOptions = { fillColor: 'red' , color: 'red'}
+
   useEffect(() => {
     if (!map) return;
   
@@ -22,10 +34,7 @@ const Map = ({childToParent}) => {
         
   return (
     <div>
-      <div>
-            
-        </div >
-      <MapContainer center={[50.447731, 30.542721]} zoom={13}
+      <MapContainer zoom={13}
         center={position}
         whenCreated={setMap}
       >
@@ -34,6 +43,13 @@ const Map = ({childToParent}) => {
           url="https://api.mapbox.com/styles/v1/andrew-app/cl02c6cxf005a15oirym7wn2a/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiYW5kcmV3LWFwcCIsImEiOiJjbDAxcjlzdWwwaTMyM3Fyc3JreDBqZGI4In0.QH_An0n-PwVGGbQGVSCWfQ"
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors Imagery © <a href="https://www.mapbox.com/">Mapbox</a>'
         />
+        
+        <>{circleData.map(({latlng,articles}) => (
+          <Circle key={latlng} center={latlng} pathOptions={fillRedOptions} radius={articles * 10}>
+            <Popup>Popup in Circle</Popup>
+          </Circle>
+        ))} </>
+
       </MapContainer>
       {childToParent(textCoords)}
     </div>
