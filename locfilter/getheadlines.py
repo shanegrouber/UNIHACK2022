4b7102db09a66e41eb35af8d13536f596ded15a7
andrewapp <andrew.appuhamy@gmail.com>
2022-02-26 17:44:27 +1100

    api call with timer

--- locfilter/getheadlines.py
+++ locfilter/getheadlines.py
@@ -1,14 +1,17 @@
 from newsapi import NewsApiClient
 import json
-# Init
-newsapi = NewsApiClient(api_key='078b5bc2774b43938740f8858f4807ad')
-
-top_sources = "the-wall-street-journal, bbc-news, reuters"
-
-# /v2/top-headlines
-top_headlines = newsapi.get_top_headlines(sources=top_sources)
 
 
-top_headlines = json.dumps(top_headlines)
-with open('top-headlines.json', 'w') as f: 
-    f.write(top_headlines)
+def fetchnews():
+
+    newsapi = NewsApiClient(api_key='078b5bc2774b43938740f8858f4807ad')
+
+    top_sources = "the-wall-street-journal, bbc-news, reuters"
+
+    # /v2/top-headlines
+    top_headlines = newsapi.get_top_headlines(sources=top_sources)
+
+
+    top_headlines = json.dumps(top_headlines)
+    with open('top-headlines.json', 'w') as f: 
+        f.write(top_headlines)
--- locfilter/parser.py
+++ locfilter/parser.py
@@ -3,78 +3,82 @@ import json
 import numpy as np
 import pandas as pd
 import simplejson
-in_file = open('top-headlines.json')
 
-data = json.load(in_file)
+def getcount():
+    in_file = open('top-headlines.json')
 
-
-titles = [i['title'] for i in data['articles']]
-
-descriptions = [i['description'] for i in data['articles']]
-
-sources = [i['source'] for i in data['articles']]
-
-content = [i['content'] for i in data['articles']]
-
-for i in range (0,len(sources)):
-    sources[i] = sources[i]['name']
-
-titles = np.array(titles)
-
-descriptions = np.array(descriptions)
-
-sources = np.array(sources)
-
-contents = np.array(content)
+    data = json.load(in_file)
 
 
-contents = contents[contents != None] #remove nonetype elements in array
+    titles = [i['title'] for i in data['articles']]
 
-df = pd.read_csv('cities.csv')
-df['count'] = 0
-cities = df['city']
+    descriptions = [i['description'] for i in data['articles']]
 
-df.set_index('city', inplace=True, drop=True)
+    sources = [i['source'] for i in data['articles']]
+
+    content = [i['content'] for i in data['articles']]
+
+    for i in range (0,len(sources)):
+        sources[i] = sources[i]['name']
+
+    titles = np.array(titles)
+
+    descriptions = np.array(descriptions)
+
+    sources = np.array(sources)
+
+    contents = np.array(content)
 
 
-for city in cities:
-    for title in titles:
-        isSource = False
-        for source in sources:
-            if source in title:
-                isSource = True
-        if city in title:
-            if isSource is False:
+    contents = contents[contents != None] #remove nonetype elements in array
+
+    df = pd.read_csv('cities.csv')
+    df['count'] = 0
+    cities = df['city']
+
+    df.set_index('city', inplace=True, drop=True)
+
+
+    for city in cities:
+        for title in titles:
+            isSource = False
+            for source in sources:
+                if source in title:
+                    isSource = True
+            if city in title:
+                if isSource is False:
+                    df.loc[city,'count'] += 1
+        
+        for desc in descriptions:
+            if city in desc:
                 df.loc[city,'count'] += 1
+        
+        for cont in contents:
+            if city in cont:
+                df.loc[city,'count'] += 1
+
+
+    df=df[df!=0].dropna()
+
+    df.reset_index(drop=False, inplace=True)
+
+    df = df.drop('Unnamed: 0', 1)
+
+
+    df.index.name = 'id'
+
+    print(df)
     
-    for desc in descriptions:
-        if city in desc:
-            df.loc[city,'count'] += 1
-    
-    for cont in contents:
-        if city in cont:
-            df.loc[city,'count'] += 1
+    out = df.to_dict('index')
+
+    obj = json.dumps(out, indent = 4)
+
+    with open("sampledata.json", "w") as outfile:
+        outfile.write(obj)
 
 
-df=df[df!=0].dropna()
-
-df.reset_index(drop=False, inplace=True)
-
-df = df.drop('Unnamed: 0', 1)
-
-df.index.name = 'id'
-
-out = df.to_dict('index')
-
-obj = json.dumps(out, indent = 4)
-
-print(obj)
-with open("sampledata.json", "w") as outfile:
-    outfile.write(obj)
-
-
-in_file.close()
-outfile.close()
+    in_file.close()
+    outfile.close()
 
 
 
