2565c09317bcc39e22c86bead10be3159289e995
tron2790 <jasonstuart36@gmail.com>
2022-02-26 20:21:24 +1030

    Update Footer.js
    Added more last names etc

--- frontend/src/Components/Footer.js
+++ frontend/src/Components/Footer.js
@@ -8,20 +8,20 @@ const Footer = () => {
                 <li className="mr-6">
                         <h2 className="text-black ">Created by</h2>
                     </li>
-                    <li className="mr-6">
-                        <h2 className="text-black transition ease-in-out  hover:-translate-y-1 hover:scale-110  duration-300">Jason Stuart</h2>
+                    <li className="mr-6 border-b-4">
+                        <h2 className="text-black transition ease-in-out  hover:-translate-y-1 hover:scale-110  duration-300 hover:shadow-xl">Jason Stuart</h2>
                     </li>
-                    <li className="mr-6">
-                        <h2 className="text-black transition ease-in-out  hover:-translate-y-1 hover:scale-110  duration-300">Nikita Zaytsev</h2>
+                    <li className="mr-6 border-b-4">
+                        <h2 className="text-black transition ease-in-out  hover:-translate-y-1 hover:scale-110  duration-300 hover:shadow-xl ">Nikita Zaytsev</h2>
                     </li>
-                    <li className="mr-6">
-                        <h2 className="text-black transition ease-in-out  hover:-translate-y-1 hover:scale-110  duration-300">Andrew</h2>
+                    <li className="mr-6 border-b-4">
+                        <h2 className="text-black transition ease-in-out  hover:-translate-y-1 hover:scale-110  duration-300 hover:shadow-xl">Andrew Appuhamy</h2>
                     </li>
-                    <li className="mr-6">
-                        <h2 className="text-black transition ease-in-out  hover:-translate-y-1 hover:scale-110  duration-300">Jeremy West</h2>
+                    <li className="mr-6 border-b-4">
+                        <h2 className="text-black transition ease-in-out  hover:-translate-y-1 hover:scale-110  duration-300 hover:shadow-xl">Jeremy West</h2>
                     </li>
-                    <li className="mr-6">
-                        <h2 className="text-black transition ease-in-out  hover:-translate-y-1 hover:scale-110  duration-300 ">Shane</h2>
+                    <li className="mr-6 border-b-4">
+                        <h2 className="text-black transition ease-in-out  hover:-translate-y-1 hover:scale-110  duration-300 hover:shadow-xl">Shane Grouber</h2>
                     </li>
                 </ul>
             </div>
