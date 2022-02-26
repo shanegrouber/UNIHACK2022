1bda7b0cca03cb6721f6a28f3c1117640d0f5e52
tron2790 <jasonstuart36@gmail.com>
2022-02-26 20:03:25 +1030

    Update Footer.js
    corrected the class name to className

--- frontend/src/Components/Footer.js
+++ frontend/src/Components/Footer.js
@@ -2,26 +2,26 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <div class="flex justify-center border-t-2 border-gray-200">
+        <div className="flex justify-center border-t-2 border-gray-200">
             <div>
-                <ul class="flex pt-2 pb-2">
-                <li class="mr-6">
-                        <h2 class="text-black ">Created by</h2>
+                <ul className="flex pt-2 pb-2">
+                <li className="mr-6">
+                        <h2 className="text-black ">Created by</h2>
                     </li>
-                    <li class="mr-6">
-                        <h2 class="text-black transition ease-in-out  hover:-translate-y-1 hover:scale-110  duration-300">Jason Stuart</h2>
+                    <li className="mr-6">
+                        <h2 className="text-black transition ease-in-out  hover:-translate-y-1 hover:scale-110  duration-300">Jason Stuart</h2>
                     </li>
-                    <li class="mr-6">
-                        <h2 class="text-black transition ease-in-out  hover:-translate-y-1 hover:scale-110  duration-300">Nikita Zaytsev</h2>
+                    <li className="mr-6">
+                        <h2 className="text-black transition ease-in-out  hover:-translate-y-1 hover:scale-110  duration-300">Nikita Zaytsev</h2>
                     </li>
-                    <li class="mr-6">
-                        <h2 class="text-black transition ease-in-out  hover:-translate-y-1 hover:scale-110  duration-300">Andrew</h2>
+                    <li className="mr-6">
+                        <h2 className="text-black transition ease-in-out  hover:-translate-y-1 hover:scale-110  duration-300">Andrew</h2>
                     </li>
-                    <li class="mr-6">
-                        <h2 class="text-black transition ease-in-out  hover:-translate-y-1 hover:scale-110  duration-300">Jeremy</h2>
+                    <li className="mr-6">
+                        <h2 className="text-black transition ease-in-out  hover:-translate-y-1 hover:scale-110  duration-300">Jeremy</h2>
                     </li>
-                    <li class="mr-6">
-                        <h2 class="text-black transition ease-in-out  hover:-translate-y-1 hover:scale-110  duration-300 ">Shane</h2>
+                    <li className="mr-6">
+                        <h2 className="text-black transition ease-in-out  hover:-translate-y-1 hover:scale-110  duration-300 ">Shane</h2>
                     </li>
                 </ul>
             </div>
