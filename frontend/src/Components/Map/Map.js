53bb2b587fee49a8eaf6d09d10331e63fb1bd93b
Jeremy West <jeremywest2000@gmail.com>
2022-02-26 17:33:32 +1100

    fixed circle meme

--- frontend/src/Components/Map/Map.js
+++ frontend/src/Components/Map/Map.js
@@ -6,6 +6,10 @@ import './Map.css';
 const Map = () => {
   const position = [50.447731, 30.542721];
   const [map, setMap] = useState(null);
+  const [coords, setCoords] = useState({});
+  const fillRedOptions = { fillColor: 'red' , color: 'red'}
+  const [circles, setCircles] = useState([]);
+
 
   // Test Data for placing circles
   const circleData = [
@@ -14,31 +18,11 @@ const Map = () => {
     {"latlng": [52, 32], "articles": 1},
     {"latlng": [49, 29], "articles": 1000},
   ];
-  const Circles = [];
-  //function DrawCircle() {
-    
-    //circleData.forEach(point => {
-    //  
-    //});
-    //return <Circle center={circleData[0].latlng} pathOptions={fillRedOptions} radius={circleData[0].articles * 10}></Circle>
-  //}
-  //ReactDOM.render(<DrawCircle />, document.getElementById('circles'))
-  function circleList() {
-    return (
-      <ol>
-        {circleData.map(point => (
-          <li key={point.latlng}>{point.latlng}</li>
-        ))}
-      </ol>
-    );
-  }
-  const [coords, setCoords] = useState({});
-  
-  const fillRedOptions = { fillColor: 'red' , color: 'red'}
 
   useEffect(() => {
+    setCircles(circleData);
+    console.log(circles)
     if (!map) return;
-  
     map.addEventListener("click", (e) => {
       setCoords({ lat: e.latlng.lat, lng: e.latlng.lng });
     });
@@ -48,16 +32,14 @@ const Map = () => {
   
   return (
     <div>
-      <MapContainer zoom={13}
-        center={position}
-        whenCreated={setMap}
-      >
+      <MapContainer zoom={13} center={position} whenCreated={setMap}>
         <TileLayer
           url="https://api.mapbox.com/styles/v1/andrew-app/cl02c6cxf005a15oirym7wn2a/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiYW5kcmV3LWFwcCIsImEiOiJjbDAxcjlzdWwwaTMyM3Fyc3JreDBqZGI4In0.QH_An0n-PwVGGbQGVSCWfQ"
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors Imagery © <a href="https://www.mapbox.com/">Mapbox</a>'
         />
-        
-        <>{circleData.map(({latlng,articles}) => (
+
+        <>
+        {circles.map(({latlng,articles}) => (
           <Circle key={latlng} center={latlng} pathOptions={fillRedOptions} radius={articles * 10}>
             <Popup>Popup in Circle</Popup>
           </Circle>
@@ -72,7 +54,6 @@ const Map = () => {
           <b>longitude</b>: {lng?.toFixed(4)}
         </div>
       )}
-      {console.log(lng, lat)}
     </div>
   );
 };
