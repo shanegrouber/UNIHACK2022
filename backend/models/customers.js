4253b16f2cfa3f11cd57323cc3d7a2a58cf13aed
Shane <66246046+shanegrouber@users.noreply.github.com>
2022-02-27 14:24:12 +1100

    Fix customer validation

--- backend/models/customers.js
+++ backend/models/customers.js
@@ -7,7 +7,7 @@ const customerSchema = new mongoose.Schema({
         required: true,
         validate: {
             validator: function(code) {
-                return !isNan(code) && code.length <= 3;
+                return !isNaN(code) && code.length <= 3;
             },
             message: "Area code must be a number and less than 3 digits."
         }
@@ -25,7 +25,7 @@ const customerSchema = new mongoose.Schema({
         required: true,
         validate: {
             validator: function(number) {
-                return !isNan(number) && number.length <= 10;
+                return !isNaN(number) && number.length <= 10;
             },
             message: "Phone number must be a number and less than 10 digits."
         }
