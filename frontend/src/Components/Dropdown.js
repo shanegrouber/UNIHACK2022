5be0a6046549fb01dd6c84df10164e74131dba3e
Jeremy West <jeremywest2000@gmail.com>
2022-02-27 15:51:33 +1100

    added about dropdown

---
+++ frontend/src/Components/Dropdown.js
@@ -0,0 +1,68 @@
+import React from "react";
+import { createPopper } from "@popperjs/core";
+
+const Dropdown = ({ color }) => {
+  // dropdown props
+  const [dropdownPopoverShow, setDropdownPopoverShow] = React.useState(false);
+  const btnDropdownRef = React.createRef();
+  const popoverDropdownRef = React.createRef();
+  const openDropdownPopover = () => {
+    createPopper(btnDropdownRef.current, popoverDropdownRef.current, {
+      placement: "bottom-start"
+    });
+    setDropdownPopoverShow(true);
+  };
+  const closeDropdownPopover = () => {
+    setDropdownPopoverShow(false);
+  };
+  // bg colors
+  let bgColor;
+  color === "white"
+    ? (bgColor = "bg-blueGray-700")
+    : (bgColor = "bg-" + color + "-500");
+  return (
+    <>
+      <div className="flex flex-wrap">
+        <div className="w-full sm:w-6/12 md:w-4/12 px-4">
+          <div className="relative inline-flex align-middle w-full">
+            <button
+              className={
+                "text-black font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150 " +
+                bgColor
+              }
+              type="button"
+              ref={btnDropdownRef}
+              onClick={() => {
+                dropdownPopoverShow
+                  ? closeDropdownPopover()
+                  : openDropdownPopover();
+              }}
+            >
+              {color === "white" ? "About" : color + " Dropdown"}
+              
+            </button>
+            <div
+              ref={popoverDropdownRef}
+              className={
+                (dropdownPopoverShow ? "block " : "hidden ") +
+                (color === "white" ? "bg-white " : bgColor + " ") +
+                "text-base z-50 float-left py-2 list-none text-left rounded shadow-lg mt-1"
+              }
+              style={{ minWidth: "12rem" }}
+            >
+            this is a section about us
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default function DropdownRender() {
+  return (
+    <>
+      <Dropdown color="white" />
+    </>
+  );
+}
